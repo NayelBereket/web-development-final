6112da7d580b0c22c9d67b06f7696389cfa41e6c
NayelBereket <156391209+NayelBereket@users.noreply.github.com>
2024-05-10 16:15:29 -0700

    Final Update For Skilled Strings
    Final Update (for now)

--- pages/page3.vue
+++ pages/page3.vue
@@ -1,7 +1,31 @@
 <template>
     <div class="container mx-auto px-4 py-8">
       <h2 class="text-3xl font-bold mb-4">Our Services</h2>
-      <p class="text-lg mb-8">Discover the guitar lesson options we offer.</p>
+      <p class="text-lg mb-8">We Offer Lessons The Following Play Styles.</p>
       <!-- Add content about services -->
+      <p class="bg-buttercup-500 text-xl font-semibold mb-8 py-4 px-6 rounded">Acoustic</p>
+      <p class="text-lg mb-8">- Fundamentals </p>
+      <p class="text-lg mb-8">- Strumming Patterns </p>
+      <p class="text-lg mb-8">- Songs </p>
+      <!-- Add content about services -->
+      <p class="bg-gull-gray-500 text-xl font-semibold mb-8 py-4 px-6 rounded">Electric</p>
+      <p class="text-lg mb-8">- Fundamentals </p>
+      <p class="text-lg mb-8">- Strumming Patterns </p>
+      <p class="text-lg mb-8">- Power Chords </p>
+      <p class="text-lg mb-8">- Songs </p>
+      <!-- Add content about services -->
+      <p class="bg-buttercup-300 text-xl font-semibold mb-8 py-4 px-6 rounded">Banjo</p>
+      <p class="text-lg mb-8">- Fundamentals </p>
+      <p class="text-lg mb-8">- Strumming Patterns </p>
+      <p class="text-lg mb-8">- Songs </p>
+      <!-- Add content about services -->
+      <p class="bg-gull-gray-300 text-xl font-semibold mb-8 py-4 px-6 rounded">Ukelele</p>  
+      <p class="text-lg mb-8">- Fundamentals </p>
+      <p class="text-lg mb-8">- Strumming Patterns </p>
+      <p class="text-lg mb-8">- Songs </p>   
+         <!-- Add content about services --> 
+      <nuxt-link to="/page3" class="bg-buttercup-300 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
+        Contact Us Here To Set Up Your First Lesson!
+      </nuxt-link>
     </div>
   </template>
