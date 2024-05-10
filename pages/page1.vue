9610d353149cdb2941516f1f9c31e7d9071e322b
NayelBereket <156391209+NayelBereket@users.noreply.github.com>
2024-05-10 15:34:48 -0700

    Updates

--- pages/page1.vue
+++ pages/page1.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="container mx-auto px-4 py-8">
       <h2 class="text-3xl font-bold mb-4">About Us</h2>
-      <p class="text-lg mb-8">At Skilled Strings, our team consists of highly skilled musicians who are not only experts in their craft but also passionate educators committed to nurturing the next generation of guitarists. With years of experience under their belts, our instructors bring a wealth of knowledge and expertise to every lesson, guiding students through a comprehensive curriculum designed to enhance their skills and ignite their musical passion.</p>
-      <nuxt-link to="/page5" class="bg-buttercup-300 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
+      <p class="text-lg mb-8">At Skilled Strings, our team consists of highly skilled musicians who are not only experts in their craft but also passionate educators committed to nurturing the next generation of guitarists. With years of experience under their belts, our instructors bring a wealth of knowledge and expertise to every lesson, guiding students through a comprehensive curriculum designed to enhance their skills and ignite their musical passion. </p>
+      <nuxt-link to="/page5" class="bg-gull-gray-500 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
         Meet Our Instructors
       </nuxt-link>
     </div>
 
     <div class="container mx-auto px-4 py-8">
-      <p class="text-lg mb-8">Skilled Strings offers personalized guitar lessons taught by professional instructors.</p>
+      <h2 class="text-3xl font-bold mb-4">Our Mission</h2>
+      <p class="text-lg mb-8">Our mission at Skilled Strings is not just to teach guitar but to inspire a lifelong love of music. We believe that learning an instrument should be an enjoyable and rewarding experience, and we strive to create a supportive and encouraging environment where students can flourish. Whether you dream of playing in a band, writing your own songs, or simply strumming your favorite tunes, Skilled Strings is here to help you achieve your musical goals.</p>
+      <p class="text-lg font-semibold mb-8">Join us at Skilled Strings and embark on an exciting musical journey filled with discovery, growth, and endless possibilities. Let's make beautiful music together! </p>
       <nuxt-link to="/page3" class="bg-gull-gray-500 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
         See Our Services
       </nuxt-link>
     </div>
+    
+    
+    <div class="container mx-auto px-4 py-8">
+      <h2 class="text-white text-3xl font-bold mb-4">Our Mission</h2>
+      <p class="text-white text-lg mb-8">Our mission at Skilled Strings is not just to teach guitar but to inspire a lifelong love of music. We believe that learning an instrument should be an enjoyable and rewarding experience, and we strive to create a supportive and encouraging environment where students can flourish. Whether you dream of playing in a band, writing your own songs, or simply strumming your favorite tunes, Skilled Strings is here to help you achieve your musical goals.</p>
+      <p class="text-white text-lg font-semibold mb-8">Join us at Skilled Strings and embark on an exciting musical journey filled with discovery, growth, and endless possibilities. Let's make beautiful music together! </p>
+    </div>
+
   </template>
--- pages/page2.vue
+++ pages/page2.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container mx-auto px-4 py-8">
       <h2 class="text-3xl font-bold mb-4">Contact Us</h2>
-      <p class="text-lg mb-4">Reach out to us to schedule your first guitar lesson!</p>
+      <p class="text-lg mb-4">Call Us!</p>
       <a href="tel:+1234567890" class="text-blue-500">Call: +1 (XXX) XXX-XXXX</a>
+      <p class="text-lg mb-4">Email Us Here!</p>
+    <a href="tel:+1234567890" class="text-blue-500">Email: learnguitar@skilledstrings.com</a>
     </div>
   </template>
