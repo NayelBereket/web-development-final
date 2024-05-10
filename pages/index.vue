42519747100db4ed70abce0a139c72c211906497
NayelBereket <156391209+NayelBereket@users.noreply.github.com>
2024-05-10 10:46:54 -0700

    Created more usability with homepage

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
     <HeroSection image="/Users/lenovo/Downloads/Skilled Strings Logo.png">
       <h1 class="text-4xl font-bold mb-4">Welcome to Skilled Strings</h1>
       <p class="text-lg mb-8">Learn guitar from experienced tutors in your local area.</p>
-      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+      <!-- Replace the button with a nuxt-link -->
+      <nuxt-link to="/page3" class="bg-buttercup-300 hover:bg-buttercup-500 text-white font-bold py-2 px-4 rounded">
         Get Started
-      </button>
+      </nuxt-link>
     </HeroSection>
   </div>
 </template>
