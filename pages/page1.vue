43e04006746aba5ab963309e7d191f19cb2f7a92
NayelBereket <156391209+NayelBereket@users.noreply.github.com>
2024-05-10 14:55:36 -0700

    Further Updates

--- pages/page1.vue
+++ pages/page1.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="container mx-auto px-4 py-8">
       <h2 class="text-3xl font-bold mb-4">About Us</h2>
-      <p class="text-lg mb-8">Welcome to Skilled Strings, where we believe that music has the power to inspire, connect, and transform lives. Founded with a passion for fostering musical talent, Skilled Strings is dedicated to providing top-tier guitar tutoring services tailored to each individual's unique journey.</p>
+      <p class="text-lg mb-8">At Skilled Strings, our team consists of highly skilled musicians who are not only experts in their craft but also passionate educators committed to nurturing the next generation of guitarists. With years of experience under their belts, our instructors bring a wealth of knowledge and expertise to every lesson, guiding students through a comprehensive curriculum designed to enhance their skills and ignite their musical passion.</p>
       <nuxt-link to="/page5" class="bg-buttercup-300 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
-        See Our Services
+        Meet Our Instructors
       </nuxt-link>
     </div>
 
     <div class="container mx-auto px-4 py-8">
       <p class="text-lg mb-8">Skilled Strings offers personalized guitar lessons taught by professional instructors.</p>
-      <nuxt-link to="/page3" class="bg-buttercup-300 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
-        Meet Our Instructors
+      <nuxt-link to="/page3" class="bg-gull-gray-500 hover:bg-buttercup-500 text-white font-bold font-lg py-4 px-6 rounded">
+        See Our Services
       </nuxt-link>
     </div>
   </template>
